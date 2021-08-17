2ab4b061a856e0ccda1a280d3a7ca5ac59e6e9fd
MinhPhuPham <phamminhphueur@gmail.com>
2021-08-17 09:18:10 +0700

    updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var events = require("events");
-exports.Proxy = function(opts) {
+var http = require('http'); // let try require("https") if wrong
+exports.Proxy = function (opts) {
 	if (!opts) throw new Error("No options provided!");
 	if (typeof opts === "string" || opts instanceof String) {
 		var url = require("url").parse(opts);
@@ -13,14 +14,14 @@ exports.Proxy = function(opts) {
 	var serverHeaders = null;
 	var clients = [];
 	/* Begin consuming stream */
-	var stream = require("http").request(opts);
+	var stream = http.request(opts);
 	stream.end();
 	/* Got a response from the stream */
-	stream.on("response", function(res) {
+	stream.on("response", function (res) {
 		serverHeaders = res.headers;
 		res.setEncoding("binary");
 		/* Send the new chunk to every client on the list */
-		res.on("data", function(chunk) {
+		res.on("data", function (chunk) {
 			for (var i = clients.length; i--;) {
 				clients[i].write(chunk, "binary");
 			}
@@ -28,21 +29,21 @@ exports.Proxy = function(opts) {
 	});
 
 	this.emitter = emitter = new events.EventEmitter();
-	stream.on("error", function(err) {
+	stream.on("error", function (err) {
 		emitter.emit("error", err);
 	});
 
 	/* Add a new client to the list and send initial headers */
-	this.requestHandler = function(req, res) {
+	this.requestHandler = function (req, res) {
 		res.writeHead(200, {
 			"Expires": "Mon, 01 Jul 1980 00:00:00 GMT",
-      "Cache-Control": "no-cache, no-store, must-revalidate",
-      "Pragma": "no-cache",
-      "Content-Type": serverHeaders["content-type"]
+			"Cache-Control": "no-cache, no-store, must-revalidate",
+			"Pragma": "no-cache",
+			"Content-Type": serverHeaders["content-type"]
 		});
 		clients.push(res);
 		emitter.emit("addClient");
-		res.socket.on("close", function() {
+		res.socket.on("close", function () {
 			clients.splice(clients.indexOf(res), 1);
 			emitter.emit("dropClient");
 		});
